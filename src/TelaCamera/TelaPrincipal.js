ec914048dea8adb9479e4b65ed2848d005937a23
nathanbarbosa <nathanbarbosa@finchsolucoes.com.br>
2022-11-29 19:56:38 -0300

    Apagar coments

--- src/TelaCamera/TelaPrincipal.js
+++ src/TelaCamera/TelaPrincipal.js
@@ -1,4 +1,3 @@
-// Import dependencies
 import React, { useRef, useState, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
 import Webcam from "react-webcam";
@@ -20,39 +19,31 @@ function TelaPrincipal() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
-  // Main function
   const runCoco = async () => {
-    // 3. TODO - Load network 
     const net = await tf.loadGraphModel('https://tcc.s3.br-sao.cloud-object-storage.appdomain.cloud/model.json')
 
-    // Loop and detect hands
     setInterval(() => {
       detect(net);
     }, 16.7);
   };
 
   const detect = async (net) => {
-    // Check data is available
     if (
       typeof webcamRef.current !== "undefined" &&
       webcamRef.current !== null &&
       webcamRef.current.video.readyState === 4
     ) {
 
-      // Get Video Properties
       const video = webcamRef.current.video;
       const videoWidth = webcamRef.current.video.videoWidth;
       const videoHeight = webcamRef.current.video.videoHeight;
 
-      // Set video width
       webcamRef.current.video.width = videoWidth;
       webcamRef.current.video.height = videoHeight;
 
-      // Set canvas height and width
       canvasRef.current.width = videoWidth;
       canvasRef.current.height = videoHeight;
 
-      // 4. TODO - Make Detections
       const img = tf.browser.fromPixels(video)
       const resized = tf.image.resizeBilinear(img, [640, 480])
       const casted = resized.cast('int32')
@@ -65,7 +56,6 @@ function TelaPrincipal() {
       const classes = await obj[6].array()
       const scores = await obj[4].array()
 
-      // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
 
       requestAnimationFrame(() => { drawRect(boxes[0], classes[0], scores[0], 0.4, videoWidth, videoHeight, ctx) });
